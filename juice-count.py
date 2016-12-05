c11dcf0c84cc556b34b4908c84514b43bcd6966b
Abhishek Modi <abhishek.modi95@gmail.com>
2016-12-05 17:26:20 -0600

    count code

--- juice-count.py
+++ juice-count.py
@@ -7,7 +7,7 @@ output_dict = {}
 for f in files:
     inp = open("data/{}".format(f), 'r')
     print("File = {}".format(inp))
-    for line in inp.readline():
+    for line in inp.readlines():
         for word in line.split():
             if word in output_dict:
                 output_dict[word] = 1
