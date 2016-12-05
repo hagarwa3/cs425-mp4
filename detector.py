b916a0464cec350baba0681a1ad0a2858f83c52a
Abhishek Modi <abhishek.modi95@gmail.com>
2016-12-05 15:26:48 -0600

    ping global

--- detector.py
+++ detector.py
@@ -7,6 +7,12 @@ class Detector():
         self.member_list = member_list
         self.name = socket.gethostbyname(socket.gethostname())
 
+    def re_join_group(self):
+        join_request = ping_message
+        introducer_connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        introducer_connection.connect((introducer_hostname, introducer_port))
+        introducer_connection.close()
+
     def get_neighbours(self):
         index = None
         try:
@@ -15,19 +21,19 @@ class Detector():
             print("joining again")
             self.re_join_group()
             return
-        
-        if (len(self.member_list) < 4):
+
+        if len(self.member_list) < 4:
             neighbors = set(self.member_list)
             neighbors.remove(self.name)
             return neighbors
-        
+
         neighbors = set()
         neighbors.add(self.member_list[index-2])
-        neighbors.add(self.member_list[index-1]) 
+        neighbors.add(self.member_list[index-1])
         neighbors.add(self.member_list[index+1])
         neighbors.add(self.member_list[index+2])
         return neighbors
-    
+
     def ping_neighbors(self):
         global ping_message
         print(self.member_list)
