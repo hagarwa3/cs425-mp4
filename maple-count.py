aa2b81628c261435f69ea75d8f4b2ed719f2808f
Abhishek Modi <abhishek.modi95@gmail.com>
2016-12-05 17:37:57 -0600

    count code

--- maple-count.py
+++ maple-count.py
@@ -2,10 +2,15 @@ import os
 
 files = os.listdir("data/")
 output_file = open('output_m.txt', "w")
-
+count = 0
 
 for f in files:
     inp = open("data/{}".format(f), 'r')
-    for line in inp.readline():
+    print("File = {}".format(inp))
+    for line in inp.readlines():
         for word in line.split():
-            output_file.write("({}, 1)\n".format(word))
+            count += 1
+            output_file.write("({}, {})\n".format(word, 1))
+
+output_file.close()
+print("Number of outputs = {}".format(count))
